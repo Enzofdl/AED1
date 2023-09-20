c308f104c352d199c2b3baa81a1d0dded4a7fc18
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2023-09-20 18:13:56 -0300

    Add files via upload

---
+++ Lab 09/Ex. A/obj/ListaC.h
@@ -0,0 +1,28 @@
+#ifndef LISTAC_H_INCLUDED
+#define LISTAC_H_INCLUDED
+
+typedef struct aluno{
+char nome[50];
+int mat;
+float n1;
+
+}aluno;
+
+typedef struct lista lista;
+
+lista* criar();
+void limpar(lista* l);
+int tamanho(lista* l);
+int inseririnicio(lista* l, aluno it);
+int inserirfim(lista* l, aluno it);
+int inserirposicao(lista* l, aluno it, int pos);
+int removerinicio(lista* l);
+int removerfim(lista* l);
+int removerposicao(lista* l, int pos);
+int buscaritem(lista* l, aluno it);
+int buscarpos(lista* l, int pos, aluno* it);
+int listavazia(lista* l);
+void mostrar(lista* l);
+
+
+#endif // LISTAC_H_INCLUDED
