1e23726ac612570ecdf9a891f3e50dcc0af92b43
Enzo Faria <enzo.lacerda@ufu.br>
2023-10-29 22:45:49 -0300

    Exercício C;

--- Lab Pilha/pilha/main.c
+++ Lab Pilha/pilha/main.c
@@ -4,19 +4,15 @@
 
 #include "pilha.h"
 
-pilha* binario(int a);
+int verificamat(char* s);
+void removepos(char* s, int pos);
 
 int main()
 {
-    int a = 4;
+    char a[20] = "(){}";
     pilha* p;
-    p = binario(a);
-    while(pilhavazia(p)!=1){
-        int aux;
-        pop_(p, &aux);
-        printf("%d", aux);
-
-    }
+    if(verificamat(a)) return printf("Eh certa");
+    return printf("Incorreta");
 
 
 
@@ -27,21 +23,33 @@ int main()
 
 
 
-
-pilha* binario(int a)
-{
- int j;
- pilha* p = criar();
- while(a > 0){
-    j = 0;
-    while(j<1 && a>0){
-        j++;
-        a--;
+void removepos(char* s, int pos){
+    for(pos; pos<strlen(s); pos++){
+        s[pos] = s[pos+1];
     }
-    push(p, j);
- }
+}
 
- return p;
+int verificamat(char* s){
+    if(strlen(s)%2 == 1) return 0;
+    if(strlen(s) == 0) return 1;
+    pilha* p = criar();
+    int aux;
+    int i, j;
+    for(i = 0; i<strlen(s); i++){
+        push(p, s[i]);
+        for(j = i+1; j<strlen(s); j++){
+            pop_(p, &aux);
+            if(aux == s[j]+1 || aux == s[j]+2){
+                removepos(s, j);
+                break;
+            }
+            
+
+        }
+        if(j >= strlen(s)) return 0;
+
+    }
 
 
+    return 1;
 }
